c658a97b8e9ae03b543edbfb33742c4c31bfc81c
jbdel <jeanbenoit.delbrouck@gmail.com>
2026-03-12 04:30:25 +0000

    Prepare public release (strip private metrics)

--- RadEval/metrics/hoppr_f1chexbert/__init__.py
+++
@@ -1,4 +0,0 @@
-try:
-    from .hoppr_f1chexbert import HopprF1CheXbert
-except Exception:
-    HopprF1CheXbert = None
--- RadEval/metrics/hoppr_f1chexbert_ct/__init__.py
+++
@@ -1,4 +0,0 @@
-try:
-    from .hoppr_f1chexbert_ct import HopprF1CheXbertCT
-except Exception:
-    HopprF1CheXbertCT = None
